110f9722b5e0b8395895039f5eb8d05719a1fcde
M. Hardi Raiz <raizhardi@gmail.com>
2021-11-21 19:07:52 +0700

    rewrite code

--- test.py
+++ test.py
@@ -1,2 +1,4 @@
-a = [1, 2, 3, 4]
-print("Hello world!", a)
+list_num = [1, 2, 3, 1, 3, 5, 4]
+list_num = sorted(list_num)
+b = dict((i, list_num.count(i)) for i in list_num)
+print("Hello world!", b)
